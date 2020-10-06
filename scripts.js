39d0dc20e075db37a11d775978ad463e5724e2cd
Ben Chan <benchan777@gmail.com>
2020-10-06 15:50:20 -0700

    add functions for new styles

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const displayFontFamily = document.querySelector('#show-font')
 const displayFontColor = document.querySelector('#show-color')
 const displayBackgroundColor = document.querySelector('#show-bg-color')
 const displayText = document.querySelector('#display')
+const displayFontWeight = document.querySelector('#show-weight')
+const displayFontStyle = document.querySelector('#show-style')
 
 // inputs
 const fontSizeInput = document.querySelector('#input-size')
@@ -11,6 +13,8 @@ const fontFamilyInput = document.querySelector('#select-font')
 const fontColorInput = document.querySelector('#input-color')
 const backgroundColorInput = document.querySelector('#input-bg-color')
 const textInput = document.querySelector('#enter-text')
+const fontWeightInput = document.querySelector('#font-weight')
+const fontStyleInput = document.querySelector('#font-style')
 
 // input event listeners
 fontSizeInput.addEventListener('input', handleInput)
@@ -18,6 +22,8 @@ fontFamilyInput.addEventListener('input', handleInput)
 fontColorInput.addEventListener('input', handleInput)
 backgroundColorInput.addEventListener('input', handleInput)
 textInput.addEventListener('input', handleInput)
+fontWeightInput.addEventListener('input', handleInput)
+fontStyleInput.addEventListener('input', handleInput)
 
 // this function handles all the inputs in the font styler form
 function handleInput() {
@@ -27,6 +33,8 @@ function handleInput() {
     const font_color = fontColorInput.value
     const background_color = backgroundColorInput.value
     const text_input = textInput.value
+    const font_weight = fontWeightInput.value
+    const font_style = fontStyleInput.value
 
     // changes and displays font size
     h1.style.fontSize = font_size + "px"
@@ -46,4 +54,12 @@ function handleInput() {
 
     // changes text
     displayText.innerHTML = text_input
+
+    // changes and displays font weight
+    h1.style.fontWeight = font_weight
+    displayFontWeight.innerHTML = font_weight
+
+    // changes and displays font style
+    h1.style.fontStyle = font_style
+    displayFontStyle.innerHTML = font_style
 }
